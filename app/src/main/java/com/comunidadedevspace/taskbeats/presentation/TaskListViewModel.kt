52b2f4a72a442c940d86e26efb84e695e2979203
DiegoCovolan <132297066+DiegoCovolan@users.noreply.github.com>
2023-12-04 12:14:54 -0300

    Create viewmodel

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListViewModel.kt
@@ -0,0 +1,70 @@
+package com.comunidadedevspace.taskbeats.presentation
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.comunidadedevspace.taskbeats.TaskBeatsApplication
+import com.comunidadedevspace.taskbeats.data.Task
+import com.comunidadedevspace.taskbeats.data.TaskDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskListViewModel(private val taskDao: TaskDao): ViewModel() {
+
+    //Livedata no ViewModel
+    val taskListLiveData: LiveData<List<Task>> = taskDao.getAll()
+
+    //CRUD
+    fun execute(taskAction: TaskAction){
+
+        when (taskAction.actionType) {
+            ActionType.DELETE.name -> deleteById(taskAction.task!!.id)
+            ActionType.CREATE.name -> insertintoDataBase(taskAction.task!!)
+            ActionType.UPDATE.name -> updateintoDataBase(taskAction.task!!)
+            ActionType.DELETE_ALL.name -> deleteAll()
+
+        }
+        }
+
+    //DELETE pelo ID
+    private fun deleteById(id: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            ///CoroutineScope(Dispatchers.IO).launch {
+            taskDao.deleteById(id)
+            }
+
+        }
+
+    //CREATE
+    private fun insertintoDataBase(task: Task){
+        viewModelScope.launch (Dispatchers.IO){
+            taskDao.insert(task)
+        }
+    }
+
+    //UPDATE
+    private fun updateintoDataBase(task: Task){
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.update(task)
+        }
+    }
+
+    //DELETE ALL
+    private fun deleteAll(){
+        viewModelScope.launch (Dispatchers.IO){
+            taskDao.deleteAll()
+            //   listFromDataBase() isso vai atualizar a lista no adapter, se não trabalhar em serie a thread pode não atualizar na hora
+        }
+    }
+
+
+    companion object {
+
+        fun create(application: Application):TaskListViewModel {
+            val dataBaseInstance = (application as TaskBeatsApplication).getAppDataBase()
+            val dao = dataBaseInstance.taskDao()
+            return TaskListViewModel(dao)
+        }
+    }
+}
